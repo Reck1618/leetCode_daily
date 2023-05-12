57a8056efaeafaebc9fa4cd2e1c359f4b23719a6
Reck1618 <ritiktyagi1.618@gmail.com>
2023-05-12 12:39:45 +0530

    binary search

---
+++ Algorithms/Searching_algos/binary_search.py
@@ -0,0 +1,28 @@
+"""
+Implement Binary Search.
+-> Binary Search is a searching algorithm for finding an element's position in a sorted array. In this approach, the element is always searched in the middle of a portion of an array.
+    - Binary search can be implemented only on a sorted list of items. If the elements are not sorted already, we need to sort them first
+
+Time - 
+Space -    
+"""
+
+def binary_search(arr, target):
+    left, right = 0, len(arr) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if arr[mid] == target:
+            return True
+        elif arr[mid] > target:
+            right = mid - 1
+        else:
+            left = mid + 1
+
+    return False         
+
+
+arr = [1,2,3,4,5,6,7,8,9,10]
+target = 6
+print(binary_search(arr, target))
