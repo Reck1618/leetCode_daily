983bcab9e0183087ef4ba2919dcdd3297bb6bf17
Ritik <ritiktyagi1.618@gmail.com>
2023-09-18 11:48:25 +0530

    Fibonacci_Numbers

---
+++ Algorithmic_Toolbox/Fibonacci_Numbers.py
@@ -0,0 +1,61 @@
+# Definition: The Fibonacci Sequence is a series of numbers where the next number is found by adding up the two numbers before it.
+
+# Algorithms to find fibonacci numbers :
+
+# Naive algorithm (time complexity: O(2^n), space complexity: O(n))
+def naive_fibonacci(n):
+    if n <= 1:
+        return n
+    else:
+        return naive_fibonacci(n-1) + naive_fibonacci(n-2)
+
+# Efficient algorithm (time complexity: O(n), space complexity: O(n))
+def efficient_fibonacci(n):
+    if n <= 1:
+        return n
+    else:
+        fib = [0, 1]
+        for i in range(2, n+1):
+            fib.append(fib[i-1] + fib[i-2])
+        return fib[n]
+
+# Efficient algorithm with memoization (time complexity: O(n), space complexity: O(n))
+def efficient_fibonacci_memoization(n, seen=None):
+    if seen is None:
+        seen = []
+    if n < 2:
+        return n
+    if n in seen:
+        return seen[n]
+    else:
+        result = efficient_fibonacci_memoization(n-1, seen) + efficient_fibonacci_memoization(n-2, seen)
+        seen.append(result)
+        return result
+
+
+# Efficient algorithm with space optimization (time complexity: O(n), space complexity: O(1))
+def efficient_fibonacci_space_optimized(n):
+    if n <= 1:
+        return n
+    else:
+        a = 0
+        b = 1
+        for i in range(2, n+1):
+            c = a + b
+            a = b
+            b = c
+        return b
+
+# Efficient algorithm with space optimization and modulo (time complexity: O(n), space complexity: O(1))
+def efficient_fibonacci_space_optimized_modulo(n):
+    if n <= 1:
+        return n
+    else:
+        a = 0
+        b = 1
+        for i in range(2, n+1):
+            c = (a + b) % 10
+            a = b
+            b = c
+        return b
+
