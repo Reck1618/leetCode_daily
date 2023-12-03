83bc94f45173ce5e53181a1af3d8ccd3e962ef81
Ritik <ritiktyagi1.618@gmail.com>
2023-12-03 11:19:59 +0530

    solved delete a bst node

---
+++ Leetcode_Problems/Trees/03_delete_node_in_bst.py
@@ -0,0 +1,59 @@
+"""
+Given a root node reference of a BST and a key, delete the node with the given key in the BST. Return the root node reference (possibly updated) of the BST.
+Basically, the deletion can be divided into two stages:
+Search for a node to remove.
+If the node is found, delete the node.
+
+Example 1:
+Input: root = [5,3,6,2,4,null,7], key = 3
+Output: [5,4,6,2,null,null,7]
+Explanation: Given key to delete is 3. So we find the node with value 3 and delete it.
+One valid answer is [5,4,6,2,null,null,7], shown in the above BST.
+Please notice that another valid answer is [5,2,6,null,4,null,7] and it's also accepted.
+
+Example 2:
+Input: root = [5,3,6,2,4,null,7], key = 0
+Output: [5,3,6,2,4,null,7]
+Explanation: The tree does not contain a node with value = 0.
+
+Example 3:
+Input: root = [], key = 0
+Output: []
+"""
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def deleteNode(self, root, key):
+        return self.delete(root, key)
+
+    def delete(self, root, key):
+        if root is None:
+            return root
+
+        if key < root.val:
+            root.left = self.delete(root.left, key)
+        elif key > root.val:
+            root.right = self.delete(root.right, key)
+        else:
+            if root.left is None:
+                return root.right
+            elif root.right is None:
+                return root.left
+
+            root.val = self.min_node(root.right).val
+            root.right = self.delete(root.right, root.val)
+
+        return root
+
+    def min_node(self, node):
+        print("world")
+        current = node
+        while current.left:
+            current = current.left
+        return current
