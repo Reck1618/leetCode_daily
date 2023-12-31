0e8933498eca02b84e471b1b16aa84ffcade6ce0
Ritik <ritiktyagi1.618@gmail.com>
2023-12-31 14:11:46 +0530

    solved subtre of another tree

--- Leetcode_Problems/Trees/12_subtree_of_another_tree.py
+++ Leetcode_Problems/Trees/12_subtree_of_another_tree.py
@@ -1,2 +1,33 @@
 """
+Given the roots of two binary trees root and subRoot, return true if there is a subtree of root with the same structure and node values of subRoot and false otherwise.
+A subtree of a binary tree tree is a tree that consists of a node in tree and all of this node's descendants. The tree tree could also be considered as a subtree of itself.
+
+Example 1:
+Input: root = [3,4,5,1,2], subRoot = [4,1,2]
+Output: true
+
+Example 2:
+Input: root = [3,4,5,1,2,null,null,null,null,0], subRoot = [4,1,2]
+Output: false
 """
+
+class Solution:
+    def isSubtree(self, root, subRoot) -> bool:
+        if not subRoot:
+            return True
+        if not root:
+            return False
+
+        if self.sameTree(root, subRoot):
+            return True
+
+        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+
+
+    def sameTree(self, p, q):
+        if not p and not q:
+            return True
+        if p and q and p.val == q.val:
+            return self.sameTree(p.left, q.left) and self.sameTree(p.right, q.right)
+        else:
+            return False
