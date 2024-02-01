271ca9622b990028304cebb96fcc4f57da37ddce
Ritik Tyagi <ritiktyagi1.618@gmail.com>
2024-02-01 08:53:09 +0530

    solved N-Queens

--- Leetcode_Problems/Backtracking/10_n_queens.py
+++ Leetcode_Problems/Backtracking/10_n_queens.py
@@ -12,3 +12,37 @@ Example 2:
 Input: n = 1
 Output: [["Q"]]
 """
+
+class Solution:
+    def solveNQueens(self, n: int):
+        col = set()
+        posDiag = set() # r + c
+        negDiag = set() # r - c
+
+        result = []
+        board = [["."] * n for i in range(n)]
+
+        def backtrack(r=0):
+            if r == n:
+                copy = ["".join(row) for row in board]
+                result.append(copy)
+                return
+
+            for c in range(n):
+                if c in col or (r + c) in posDiag or (r - c) in negDiag:
+                    continue
+
+                col.add(c)
+                posDiag.add(r + c)
+                negDiag.add(r - c)
+                board[r][c] = "Q"
+
+                backtrack(r + 1)
+
+                col.remove(c)
+                posDiag.remove(r + c)
+                negDiag.remove(r - c)
+                board[r][c] = "."
+
+        backtrack()
+        return result
