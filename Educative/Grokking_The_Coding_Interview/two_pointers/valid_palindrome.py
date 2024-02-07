cc4a9b1085095409ed798eba01f1420f8d2ff498
Ritik Tyagi <ritiktyagi1.618@gmail.com>
2024-02-07 07:33:20 +0530

    solved valid palindrome

--- Educative/Grokking_The_Coding_Interview/two_pointers/valid_palindrome.py
+++ Educative/Grokking_The_Coding_Interview/two_pointers/valid_palindrome.py
@@ -1,3 +1,28 @@
 """
 Write a function that takes a string, s, as an input and determines whether or not it is a palindrome.
 """
+
+def is_palindrome(s):
+
+  # Replace this placeholder return statement with your code
+  l, r = 0, len(s) - 1
+
+  while l <= r:
+    if s[l] != s[r]:
+      return False
+    l += 1
+    r -= 1
+  return True
+
+# Driver code
+def main():
+    test_cases = ["RACECAR", "ABBA", "TART"]
+    i = 1
+    for s in test_cases:
+        print("Test Case #", i)
+        print(is_palindrome(s))
+        print("-" * 100, end="\n\n")
+        i = i + 1
+
+if __name__ == '__main__':
+    main()
