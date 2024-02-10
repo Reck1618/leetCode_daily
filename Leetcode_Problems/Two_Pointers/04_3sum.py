9606f27b622fce225cddf687d899dec67a5474d8
Ritik Tyagi <ritiktyagi1.618@gmail.com>
2024-02-10 08:31:53 +0530

    solved 3sum

--- Leetcode_Problems/Two_Pointers/04_3sum.py
+++ Leetcode_Problems/Two_Pointers/04_3sum.py
@@ -2,11 +2,10 @@
 Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
 Notice that the solution set must not contain duplicate triplets.
 
- 
 Example 1:
 Input: nums = [-1,0,1,2,-1,-4]
 Output: [[-1,-1,2],[-1,0,1]]
-Explanation: 
+Explanation:
 nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
 nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
 nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
@@ -23,8 +22,9 @@ Input: nums = [0,0,0]
 Output: [[0,0,0]]
 Explanation: The only possible triplet sums up to 0.
 """
+
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
+    def threeSum(self, nums):
         nums.sort()
         ans = []
         for  i in range(len(nums)-2):
@@ -46,7 +46,7 @@ class Solution:
             if total == 0:
                 temp.append([cur, nums[l], nums[r]])
                 l += 1
-                r -= 1  
+                r -= 1
 
                 # To Avoide Duplication
                 while l < r and nums[l] == nums[l-1]:
@@ -58,5 +58,5 @@ class Solution:
                 r -= 1
             else:
                 l += 1
-  
+
         return temp
