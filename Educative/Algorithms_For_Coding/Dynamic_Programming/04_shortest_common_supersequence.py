d1ee3fab403cabc9a522961adedf964a5ef2c815
Ritik Tyagi <ritiktyagi1.618@gmail.com>
2024-02-21 17:23:51 +0530

    removed dublicate solution

--- Educative/Algorithms_For_Coding/Dynamic_Programming/04_shortest_common_supersequence.py
+++ Educative/Algorithms_For_Coding/Dynamic_Programming/04_shortest_common_supersequence.py
@@ -48,24 +48,3 @@ def shortest_common_supersequence_length(s1, s2):
                 dp[i][j] = 1 + min(dp[i - 1][j], dp[i][j - 1])
 
     return dp[len(s1)][len(s2)]
-
-
-def shortest_common_supersequence(s1, s2):
-    """
-    Finds the shortest common super sequence length
-    :param s1: First string
-    :param s2: Second string
-    :return: Length of shortest common superstring
-    """
-
-    dp = [[0 for _ in range(len(s2) + 1)] for _ in range(len(s1) + 1)]
-
-    for i in range(1, len(s1) + 1):
-        for j in range(1, len(s2) + 1):
-            if s1[i - 1] == s2[j - 1]:
-                dp[i][j] = 1 + dp[i-1][j-1]
-            else:
-                dp[i][j] = max(dp[i-1][j], dp[i][j-1])
-
-    return len(s1) + len(s2) - (dp[len(s1)][len(s2)])
-
